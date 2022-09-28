2c37325ec8ebda64dffe9ab8057fb486a64b93de
Anders Jonsson <aj224rj@student.lnu.se>
2022-09-28 12:18:26 +0200

    Fix checkstyle-issues

--- app/src/main/java/model/domain/ItemIterator.java
+++ app/src/main/java/model/domain/ItemIterator.java
@@ -1,6 +1,12 @@
 package model.domain;
 
+/**
+ * Interface used to implement the GOF Iterator-pattern.
+ * The pattern is then used in search-queries.
+ *
+ */
 public interface ItemIterator {
   public boolean hasNext();
+
   public Item next();
 }
--- app/src/main/java/model/domain/LendingContract.java
+++ app/src/main/java/model/domain/LendingContract.java
@@ -1,12 +1,24 @@
 package model.domain;
 
+/**
+ * Class to form a lending contract with all details.
+ *
+ */
 public class LendingContract {
   private int startDay;
   private int endDay;
   private Item item;
   private Member lender;
 
-  public LendingContract (Member lender, int endDay, Item item, int startDay) {
+  /**
+   * Instanciate a lending contract with the data presented as parameters.
+   *
+   * @param lender - Used as a reference to a member whom to pay credits for the total lending-fee.
+   * @param endDay - The day after the endDay the item is available for loan again.
+   * @param item - The item holds information about the owner to recieve credits for the loan.
+   * @param startDay - From this day the item is reserved until the day after endDay.
+   */
+  public LendingContract(Member lender, int endDay, Item item, int startDay) {
     this.lender = lender;
     this.endDay = endDay;
     this.item = item;
