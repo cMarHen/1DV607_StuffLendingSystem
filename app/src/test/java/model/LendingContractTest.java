d4b8a37784fb5b697ffdaf6a1a18a1e2356efbdb
Martin Henriksson <mh225wd@student.lnu.se>
2022-10-03 10:28:03 +0200

    Add Test suites for LendingContract och Member

---
+++ app/src/test/java/model/LendingContractTest.java
@@ -0,0 +1,41 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import model.domain.Item;
+import model.domain.Item.ItemType;
+import model.domain.LendingContract;
+import model.domain.Member;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test LendingContract.
+ */
+public class LendingContractTest {
+  Member newMember = new Member("t", "t", "t", "t", "t", 0);
+  Item item = new Item(newMember, ItemType.Game, "test", "test", "test_id", 0, 2);
+  LendingContract contract = new LendingContract(newMember, 3, item, 0);
+
+  @Test
+  public void createNewContract() {
+    assertTrue(contract != null);
+  }
+
+  @Test
+  public void readLenderOfContract() {
+    assertTrue(contract.getLender() != null);
+    assertTrue(contract.getLender().getId().equals(newMember.getId()));
+  }
+
+  @Test
+  public void readStartAndEndDayOfContract() {
+    assertTrue(contract.getStartDay() == 0);
+    assertTrue(contract.getEndDay() == 3);
+  }
+
+  @Test
+  public void shouldNotBeAbleToChangeLenderInfo() {
+    contract.getLender().setFirstName("newName");
+    assertTrue(newMember.getFirstName().equals("t"));
+  }
+}
