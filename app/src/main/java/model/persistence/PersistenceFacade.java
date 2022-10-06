b763aef2d0b98222b2aae35dc4fac9c1fe54e475
Martin Henriksson <mh225wd@student.lnu.se>
2022-10-06 17:57:42 +0200

    Implement load and save in PersistentFacade

--- app/src/main/java/model/persistence/PersistenceFacade.java
+++ app/src/main/java/model/persistence/PersistenceFacade.java
@@ -1,6 +1,5 @@
 package model.persistence;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.ArrayList;
 import model.domain.ContractCollection;
 import model.domain.ContractCollectionImpl;
@@ -34,14 +33,19 @@ public class PersistenceFacade {
   /**
    * Get MemberCollection, populated with members from storage.
    */  
-  @SuppressFBWarnings(value = "EI_EXPOSE_REP", justification = "Want to keep the reference")
   public MemberCollection getMemberCollection() {
     ArrayList<MemberDto> members = mapperFactory.getMemberMapper().getAll();
 
-    // TRANSFORM FROM MEMBERDTO to Member.Mutable
-
-    for (Member.Mutable m : members) {
-      memberCollection.addMember(m);
+    for (MemberDto dto : members) {
+      Member.Mutable newMember = new Member.Mutable(
+          dto.getFirstName(),
+          dto.getLastName(),
+          dto.getEmail(),
+          dto.getPhoneNumber(),
+          dto.getId(),
+          dto.getRegistredDay(),
+          dto.getCredits());
+      memberCollection.addMember(newMember);
     }
     return memberCollection;
   }
@@ -49,12 +53,21 @@ public class PersistenceFacade {
   /**
    * Get ItemCollection, populated with items from storage.
    */
-  @SuppressFBWarnings(value = "EI_EXPOSE_REP", justification = "Want to keep the reference")
   public ItemCollection getItemCollection() {
-    ArrayList<Item.Mutable> items = mapperFactory.getItemMapper().loadAll();
+    ArrayList<ItemDto> items = mapperFactory.getItemMapper().getAll();
     
-    for (Item.Mutable i : items) {
-      itemCollection.addItem(i);
+    for (ItemDto dto : items) {
+      Item.Mutable newItem = new Item.Mutable(
+          findMemberById(dto.getOwnerId()),
+          Item.ItemType.valueOf(dto.getType()),
+          dto.getName(),
+          dto.getDescription(),
+          dto.getId(),
+          dto.getDayOfCreation(),
+          dto.getCostPerDay(),
+          dto.getIsReserved());
+
+      itemCollection.addItem(newItem);
     }
     return itemCollection;
   }
@@ -62,13 +75,104 @@ public class PersistenceFacade {
   /**
    * Get ContractCollection, populated with contracts from storage.
    */
-  @SuppressFBWarnings(value = "EI_EXPOSE_REP", justification = "Want to keep the reference")
   public ContractCollection getContractCollection() {
-    ArrayList<LendingContract> contracts = mapperFactory.getContractMapper().loadAll();
-    
-    for (LendingContract c : contracts) {
-      contractCollection.addContract(c);
+    ArrayList<ContractDto> contracts = mapperFactory.getContractMapper().getAll();
+
+    for (ContractDto dto : contracts) {
+      LendingContract newContract = new LendingContract(
+          findMemberById(dto.getLenderId()),
+          dto.getEndDay(),
+          findItemById(dto.getItemId()),
+          dto.getStartDay());
+
+      contractCollection.addContract(newContract);
     }
+
     return contractCollection;
+  }
+
+  private Item findItemById(String itemId) {
+    return itemCollection.findItemById(itemId);
+  }
+
+  private Member.Mutable findMemberById(String ownerId) {
+    return memberCollection.findMemberById(ownerId);
+  }
+
+  /**
+   * Save all current data stored in collection.
+   */
+  public void save() {
+    saveAllMembers();
+    saveAllItems();
+    saveAllContracts();
+  }
+
+  private void saveAllMembers() {
+    ArrayList<MemberDto> memberDtos = new ArrayList<>();
+    Iterable<Member.Mutable> domainMembers = memberCollection.getMembers();
+
+    // Save all members
+    for (Member.Mutable m : domainMembers) {
+      MemberDto dto = new MemberDto(
+          m.getFirstName(),
+          m.getLastName(),
+          m.getEmail(),
+          m.getPhoneNumber(),
+          m.getId(),
+          m.getCredits(),
+          m.getRegistredDay());
+
+      memberDtos.add(dto);
+    }
+
+    
+  }
+
+  private void saveAllItems() {
+    ArrayList<ItemDto> itemDtos = new ArrayList<>();
+    Iterable<Item.Mutable> domainItems = itemCollection.getAllItems();
+    
+    for (Item.Mutable i : domainItems) {
+      ItemDto dto = new ItemDto(
+          i.getOwner().getId(),
+          i.getType().toString(),
+          i.getName(),
+          i.getDescription(),
+          i.getId(),
+          i.getDayOfCreation(),
+          i.getCostPerDay(),
+          i.getIsReserved());
+        
+      itemDtos.add(dto);
+    }
+  }
+    
+  private void saveAllContracts() {
+    ArrayList<ContractDto> contractDtos = new ArrayList<>();
+    ArrayList<LendingContract> domainContracts = new ArrayList<>();
+    Iterable<Item.Mutable> domainItems = itemCollection.getAllItems();
+
+    // Get domain contracts
+    for (Item.Mutable item : domainItems) {
+      ArrayList<LendingContract> contracts = contractCollection.getContractsByItem(item);
+      if (contracts.size() > 0) {
+        for (LendingContract c : contracts) {
+          domainContracts.add(c);
+        }
+      }
+    }
+
+    // Create contractDtos
+    for (LendingContract c : domainContracts) {
+      ContractDto newContract = new ContractDto(
+          c.getStartDay(),
+          c.getEndDay(),
+          c.getTotalContractFee(),
+          c.getItem().getId(),
+          c.getLender().getId());
+
+      contractDtos.add(newContract);
+    }
   }
 }
